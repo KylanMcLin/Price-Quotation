f15a57843f09041b2810d77e8129a2d3e8495023
Kylan McLin <kylanmclin@gmail.com>
2022-04-21 15:23:41 -0500

    Initial commit

---
+++ XEx04Quotation/Confirm.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XEx04Quotation
+{
+    public partial class Confirm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+
+            if (Session["salesPrice"] == null)
+            {
+                
+            }
+            else
+            {
+                lblSalesPrice.Text = Convert.ToString(Session["salesPrice"]);
+                lblDiscountAmount.Text = Convert.ToString(Session["discountAmount"]);
+                lblTotalPrice.Text = Convert.ToString(Session["totalPrice"]);
+            }
+            
+        }
+
+        protected void sendQuotButton_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                lblMessage.Text = String.Format("Quation sent to {0} at {1}.", txtName.Text, txtEmail.Text);
+                Session["salesPrice"] = null;
+                Session["discountAmount"] = null;
+                Session["totalPrice"] = null;
+            }
+
+        }
+    }
+}
---
+++ XEx04Quotation/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XEx04Quotation
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                decimal salesPrice = Convert.ToDecimal(txtSalesPrice.Text);
+                decimal discountPercent = Convert.ToDecimal(txtDiscountPercent.Text) / 100;
+
+                decimal discountAmount = salesPrice * discountPercent;
+                decimal totalPrice = salesPrice - discountAmount;
+
+                lblDiscountAmount.Text = discountAmount.ToString("c");
+                lblTotalPrice.Text = totalPrice.ToString("c");
+
+                Session["salesPrice"] = salesPrice.ToString("c");
+                Session["discountAmount"] = discountAmount.ToString("c");
+                Session["totalPrice"] = totalPrice.ToString("c");
+            }   
+        }
+
+        protected void confirmButton_Click(object sender, EventArgs e)
+        {
+            if (Session["salesPrice"] == null)
+            {
+                lblMessage.Text = "Click the Calculate button before you confirm.";
+            }
+            else
+            {
+                Response.Redirect("~/Confirm.aspx");
+            }
+        }
+    }
+}
